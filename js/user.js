3ada26a186c8d1bd5126b21118b659053068f3f8
enzoM97 <enzomarecos@gmail.com>
2018-11-15 19:09:31 -0300

    first commit

---
+++ js/user.js
@@ -0,0 +1,26 @@
+/**
+*
+* Class that represents a user
+*
+**/
+
+var User = class{
+	constructor(id, name, phone_number, birth_date, role){
+		this.id = id;
+		this.name = name;
+		this.phone_number = phone_number;
+		this.birth_date = birth_date;
+	}
+	getId(){
+		return this.id;
+	}
+	getName(){
+		return this.name;
+	}
+	getPhoneNumber(){
+		return this.phone_number;
+	}
+	getBirthDate(){
+		return this.birth_date;
+	}
+}
